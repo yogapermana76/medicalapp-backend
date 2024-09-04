f0334ea307e6394a92f3b14714468f006019c296
Yoga Permana <permana76.yp@gmail.com>
2024-09-05 00:59:00 +0700

    fix: error order

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
+  imports: [PrismaModule],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
