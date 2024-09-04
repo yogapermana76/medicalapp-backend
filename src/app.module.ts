4e32da49877951c7726aa5bb063aca3f099c3092
Yoga Permana <permana76.yp@gmail.com>
2024-09-04 18:17:11 +0700

    chore: config - prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       expandVariables: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
