2214749aca5a41d1024d05b8a2f1733aec52eb20
Yoga Permana <permana76.yp@gmail.com>
2024-09-07 19:33:53 +0700

    chore: chat: add ApiProperty to chat and message dto

--- src/modules/chat/dto/create-chat.dto.ts
+++ src/modules/chat/dto/create-chat.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class CreateChatDto {
   @IsInt()
   @IsNotEmpty()
+  @ApiProperty()
   user_id: number;
 
   @IsInt()
   @IsNotEmpty()
+  @ApiProperty()
   doctor_id: number;
 }
--- src/modules/chat/dto/create-message.dto.ts
+++ src/modules/chat/dto/create-message.dto.ts
@@ -4,10 +4,12 @@ import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 export class CreateMessageDto {
   @IsInt()
   @IsNotEmpty()
+  @ApiProperty()
   chat_id: number;
 
   @IsInt()
   @IsNotEmpty()
+  @ApiProperty()
   sender_id: number;
 
   @IsString()
